e2b2e21d2840504f0a701f94b1465a43c0e63da5
Ephraim Härer <ephraim.haerer@renolit.com>
2019-10-07 11:32:32 +0200

    updated tsconfig and fixed wrong lanugage fr to ru and updated deprecated TypoScript

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,12 +2,19 @@
 defined('TYPO3_MODE') or die();
 
 $initialize = function ($extKey) {
+    $isVersion9Up = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 9000000;
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/Extbase/setup.txt">'
     );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
-    );
+    if ($isVersion9Up) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest_9.tsconfig">'
+        );
+    } else {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
+        );
+    }
 };
 $initialize(\Mselbach\StaticInfoTablesRu\Extension::EXTENSION_KEY);
 unset($initialize);
