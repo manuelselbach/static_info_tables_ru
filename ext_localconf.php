7ade91469da338f5b7e771904d1dcd79cb3325d6
Ephraim Härer <ephraim@ephespage.de>
2019-10-13 18:08:48 +0200

    removed not needed version compare line

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,6 @@
 defined('TYPO3_MODE') or die();
 
 $initialize = function ($extKey) {
-    $isVersion9Up = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 9000000;
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/Extbase/setup.txt">'
     );
