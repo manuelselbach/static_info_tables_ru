7639fad912f4cdf32092ca47de65c8b480bba82f
Manuel Selbach <manuel_selbach@yahoo.de>
2021-12-12 19:58:23 +0100

    [TASK] Remove obsolete PageTSconfig configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,19 +1,12 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$initialize = function ($extKey) {
+(function ($extKey) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/TypoScript/Extbase/setup.txt">'
     );
-    if (version_compare(TYPO3_branch, '9.5', '>=')) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
-        );
-    } else {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest_prior_9.tsconfig">'
-        );
-    }
-};
-$initialize(\Mselbach\StaticInfoTablesRu\Extension::EXTENSION_KEY);
-unset($initialize);
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extKey . '/Configuration/PageTSconfig/Suggest.tsconfig">'
+    );
+})(\Mselbach\StaticInfoTablesRu\Extension::EXTENSION_KEY);
