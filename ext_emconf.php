6aa88f74f24f3db8d111a15dbe03db569a05dee9
Manuel Selbach <manuel_selbach@yahoo.de>
2021-12-12 19:59:17 +0100

    [TASK] Update constraints to PHP, EXT:static_info_tables and TYPO3
    With this change the dependencies are updated and also the tests are
    adapted accordingly. Like EXT:static_info_tables with this version only one
    TYPO3 version is supported.

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,8 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Static Info Tables (ru)',
-    'description' => '(ru) language pack for the Static Info Tables providing localized names for countries, 
-                       currencies and so on.',
+    'description' => '(ru) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
     'category' => 'misc',
     'version' => '6.9.0',
     'state' => 'stable',
@@ -14,9 +13,9 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'manuel_selbach@yahoo.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
-            'static_info_tables' => '6.9.0-6.9.99',
-            'php' => '7.2.0-0.0.0',
+            'typo3' => '11.5.0-11.5.99',
+            'static_info_tables' => '11.5.0-11.5.99',
+            'php' => '7.4.0-0.0.0',
         ],
         'conflicts' => [],
         'suggests' => [],
